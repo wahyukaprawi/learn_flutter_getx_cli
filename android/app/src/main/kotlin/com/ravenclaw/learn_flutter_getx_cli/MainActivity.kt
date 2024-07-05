5874328d7b13b2150141793ddc0b56cfa41b32c6
wahyukaprawi <wahyukaprawi123@gmail.com>
2024-07-05 15:29:43 +0800

    add code

---
+++ android/app/src/main/kotlin/com/ravenclaw/learn_flutter_getx_cli/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ravenclaw.learn_flutter_getx_cli
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+part of 'app_pages.dart';
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+}
